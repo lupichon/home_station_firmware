de12315392cfe3612cbe49c7aa5fd2fd69ee606b
lupichon <lucaspichon444@gmail.com>
2026-07-27 14:38:44 +0200

    mise à jour du serializer pour envoyer les données des nouveaux capteurs

--- src/communication/data_serializer.hpp
+++ src/communication/data_serializer.hpp
@@ -4,7 +4,9 @@
 #include <string.h>
 #include "../core/measurement.hpp"
 
-constexpr size_t BUFFER_SIZE = (sizeof(float) * 5 + sizeof(uint8_t));
+constexpr size_t NB_FLOATS = 4;
+constexpr size_t NB_BOOL   = 3;
+constexpr size_t BUFFER_SIZE = (sizeof(float) * NB_FLOATS + sizeof(uint8_t) * NB_BOOL);
 
 size_t serialize(const Measurement& measurement, uint8_t* buffer, size_t bufferSize)
 {
@@ -21,9 +23,6 @@ size_t serialize(const Measurement& measurement, uint8_t* buffer, size_t bufferS
     memcpy(buffer + offset, &measurement.humidity, sizeof(float));
     offset += sizeof(float);
 
-    memcpy(buffer + offset, &measurement.pressure, sizeof(float));
-    offset += sizeof(float);
-
     memcpy(buffer + offset, &measurement.co2, sizeof(float));
     offset += sizeof(float);
 
@@ -31,6 +30,12 @@ size_t serialize(const Measurement& measurement, uint8_t* buffer, size_t bufferS
     offset += sizeof(float);
 
     buffer[offset] = measurement.motion ? 1 : 0;
+    offset += sizeof(uint8_t);
+
+    buffer[offset] = measurement.sound ? 1 : 0;
+    offset += sizeof(uint8_t);
+
+    buffer[offset] = measurement.obstacle ? 1 : 0;
     offset += sizeof(uint8_t);
 
     return offset;
--- src/core/measurement.hpp
+++ src/core/measurement.hpp
@@ -6,7 +6,6 @@ struct Measurement
 {
     float temperature = NAN;
     float humidity    = NAN;
-    float pressure    = NAN;
     float co2         = NAN;
     float luminosity  = NAN;
     bool  motion      = false;
